60eeaa09c6c2bef23aff588e91f1830ae63b3d06
MaxCaulfield <Maxfromsea36@gmail.com>
2023-06-08 17:48:03 +0800

    修正AppointmentController當中Update方法的Dto處理邏輯錯誤。

--- HomeTherapistApi/Controllers/AppointmentController.cs
+++ HomeTherapistApi/Controllers/AppointmentController.cs
@@ -51,7 +51,7 @@ namespace HomeTherapistApi.Controllers
     }
 
     [HttpPatch("{IdNumber}")]
-    public async Task<IActionResult> Update([FromBody] JsonPatchDocument<Appointment> patchDocument, string IdNumber, [FromQuery] string Phone, [FromQuery] DateTime? date)
+    public async Task<IActionResult> Update([FromBody] JsonPatchDocument<AppointmentUpdateDto> patchDocument, string IdNumber, [FromQuery] string Phone, [FromQuery] DateTime? date)
     {
       var appointment = await _context.Appointments
           .Include(a => a.AppointmentDetails)
@@ -60,15 +60,70 @@ namespace HomeTherapistApi.Controllers
       if (appointment == null)
         return NotFound(new ApiResponse<object> { IsSuccess = false, Message = $"更新身分證字號為 '{IdNumber}'、電話號碼為 '{Phone}' 且日期為 '{date}' 的預約失敗。" });
 
-      patchDocument.ApplyTo(appointment, ModelState);
+      var appointmentDto = new AppointmentUpdateDto
+      {
+        CustomerId = appointment.CustomerId,
+        CustomerPhone = appointment.CustomerPhone,
+        CustomerAddress = appointment.CustomerAddress,
+        Gender = appointment.Gender,
+        AgeGroup = appointment.AgeGroup,
+        Note = appointment.AppointmentDetails.FirstOrDefault()?.Note
+      };
+
+      patchDocument.ApplyTo(appointmentDto, ModelState);
 
       if (!ModelState.IsValid)
-        return BadRequest(new ApiResponse<object> { IsSuccess = false, Message = $"提供的資料無效，無法更新身分證字號為 '{IdNumber}'、電話號碼為 '{Phone}' 且日期為 '{date}' 的預約。" });
+      {
+        var errors = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
+        return BadRequest(new ApiResponse<object> { IsSuccess = false, Message = $"提供的資料無效，無法更新身分證字號為 '{IdNumber}'、電話號碼為 '{Phone}' 且日期為 '{date}' 的預約。", Data = errors });
+      }
+
+      appointment.CustomerId = appointmentDto.CustomerId;
+      appointment.CustomerPhone = appointmentDto.CustomerPhone;
+      appointment.CustomerAddress = appointmentDto.CustomerAddress;
+      appointment.Gender = appointmentDto.Gender;
+      appointment.AgeGroup = appointmentDto.AgeGroup;
+      appointment.AppointmentDetails.FirstOrDefault()!.Note = appointmentDto.Note;
+
+      appointment.UpdatedAt = DateTime.UtcNow;
 
       await _context.SaveChangesAsync();
 
       return Ok(new ApiResponse<object> { IsSuccess = true, Message = $"更新身分證字號為 '{IdNumber}'、電話號碼為 '{Phone}' 且日期為 '{date}' 的預約已完成。" });
     }
+    // [
+    //   {
+    //     "op": "replace",
+    //     "path": "/customerId",
+    //     "value": "新身分證字號"
+    //   },
+    //   {
+    //     "op": "replace",
+    //     "path": "/customerPhone",
+    //     "value": "新電話號碼"
+    //   },
+    //   {
+    //   "op": "replace",
+    //     "path": "/customerAddress",
+    //     "value": "新地址"
+    //   },
+    //   {
+    //   "op": "replace",
+    //     "path": "/gender",
+    //     "value": "新性別"
+    //   },
+    //   {
+    //   "op": "replace",
+    //     "path": "/ageGroup",
+    //     "value": "新年齡層"
+    //   },
+    //   {
+    //   "op": "replace",
+    //     "path": "/note",
+    //     "value": "新備註"
+    //   }
+    // ]
+
 
     [HttpDelete("{IdNumber}")]
     public async Task<IActionResult> Delete(string IdNumber, string Phone, DateTime? date)
@@ -87,5 +142,13 @@ namespace HomeTherapistApi.Controllers
       return Ok(new ApiResponse<object> { IsSuccess = true, Message = $"刪除身分證字號為為 '{IdNumber}'、電話號碼為 '{Phone}' 且日期為 '{date}' 的預約已完成。" });
     }
   }
-
+  public partial class AppointmentUpdateDto
+  {
+    public string CustomerId { get; set; } = null!;
+    public string CustomerPhone { get; set; } = null!;
+    public string CustomerAddress { get; set; } = null!;
+    public string? Gender { get; set; }
+    public string? AgeGroup { get; set; }
+    public string Note { get; set; }
+  }
 }
